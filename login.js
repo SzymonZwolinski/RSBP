f22b1fa08d46c9f24b6c9fbc0dce798a8f59d088
Szymon Zwoliński <szymzwoo@gmail.com>
2022-04-10 17:57:16 +0200

    v1 Wizualizacja logowania

--- login.js
+++ login.js
@@ -1,25 +1,30 @@
+
 function validate()
 {
-    var username=document.getElementsById("Login").value;
-    var password=document.getElementsById("psw").value;
+    var username=document.getElementById("login").value;
+    var password=document.getElementById("psw").value;
     if(username=="admin"&& password=="admin")
     {
-        alert("Zalogowano jako administrator")
-        closeForm();
+        zalog();
         return false;
     }
     else
     {
-        alert("Wprowadzono złe dane")
+        alert("Wprowadzono złe dane");
     }
 }
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
   
-  function closeForm() {
+function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
-  jQuery(window).load(function() {
-  sessionStorage.setItem('status','loggedIn') 
- });
+}
+function zalog() {
+    jQuery(window).load(function() {
+        sessionStorage.setItem('status','loggedIn') 
+       });
+    alert("Zalogowano jako administrator");
+    for (let el of document.querySelectorAll('.appBanner')) el.style.visibility = 'hidden';
+
+}   
